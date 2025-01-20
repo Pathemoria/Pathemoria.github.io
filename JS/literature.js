d88d0059f4b54d3fa143a92c3f5fb3e99666ca5a
Almir Nihadović Islamović <almir.islamovic1234@gmail.com>
2025-01-20 03:17:58 +0100

    literature

--- JS/literature.js
+++ JS/literature.js
@@ -1,27 +1,34 @@
 const novels = document.querySelectorAll('[id^="segment1_grid_"]');
 const contents = document.querySelectorAll('[id^="segment1_grid_content_"]');
 let reserve = 0;
+
 novels.forEach((novel, index) => {
     reserve = index;
     novel.addEventListener('mouseover', () => {
         novel.style.width = '100vh';
-        novel.style.height = '50vh';
+        novel.style.height = '55vh';
         contents.forEach((content, reserve) => {
-            content.style.width = '54vh';
-            content.style.height = '40vh';
+            content.style.width = '90%';
+            content.style.height = '70%';
             content.style.opacity = '100%'
+            content.style.transition = "opacity 1.8s ease";
         });
     });
     novel.addEventListener('mouseout', () => {
-        novel.style.width = '30vh';
-        novel.style.height = '4vh';
-        
+        novel.style.width = '33vh';
+        novel.style.height = '4vh';        
         contents.forEach((content, reserve) => {
-            content.style.width = '0vh';
-            content.style.height = '0vh';
             content.style.opacity = '0%';
         });
     
     });
 });
+function testIn() {
+    console.log("hovering in sidebar");
+}
+  
+function testOut() {
+    console.log("hovering outside sidebar");
+}
+
 
